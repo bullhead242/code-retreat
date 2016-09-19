27c117cf4f8ffbd96a40c8b5ba387b5640bb3f8b
bullhead242 <bullhead242@gmail.com>
2016-09-19 14:37:51 -0400

    Beers writes logic around cell death & life, etc

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,4 +1,4 @@
-# cool
+#cool
 # Dan's matrix extravaganza!!!
 # matrix library for Ruby
 # http://ruby-doc.org/stdlib-2.0.0/libdoc/matrix/rdoc/Matrix.html#method-c-build
@@ -18,17 +18,22 @@ class World
 end
 
 class Cell
+  attr_accessor :state
+
 	def initialize
     @state
   	@row
     @column
   end
 
-
+  # returns if cell is alive
+  def alive?
+    state == 'live'
+  end
 
   # count live neighbors (assume access to a neighbors array)
   def count_live_neighbors
-    neighbors.filter().reduce(:+)
+    neighbors.filter(alive?).reduce(:+)
   end
 
   def find_neighbors
@@ -39,4 +44,15 @@ class Cell
 
 
   end
+
+  def to_die?
+    if count_live_neighbors > 3 || count_live_neighbors < 2
+      state = 'die'
+    end
+  end
+
+  def to_be_born?
+    if count_live_neighbors == 3
+      state = 'live'
+    end
 end
