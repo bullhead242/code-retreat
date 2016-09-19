b34c6f4dc64bd053814f56eca7c208268e899bc4
bullhead242 <bullhead242@gmail.com>
2016-09-19 14:27:18 -0400

    Add comments and neighbor definitions

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,15 +1,32 @@
-#pry
+#cool
+# Dan's matrix extravaganza!!!
+# matrix library for Ruby
+# http://ruby-doc.org/stdlib-2.0.0/libdoc/matrix/rdoc/Matrix.html#method-c-build
+
+require 'matrix'
 
 class World
   def initialize(length)
     @cells = Matrix.build(length,length) { |row, column| Cell.new }
   end
+
+  # test code for neighborhood
+  def find_neighbors
+
+  end
 end
 
 class Cell
-  def initialize
-  @state
-  @row
-  @column
+	def initialize
+    @state
+  	@row
+    @column
+  end
+
+  def find_neighbors
+    # world is a matrix
+    # world.[](x, y) will give you an individual cell with coordinates x and y
+    # world.index(cell) will return coordinates of that cell in the form [x, y]
+    
   end
 end
