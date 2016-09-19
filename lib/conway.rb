d55b8dd044a342e1b059a0cf027c50f7ccf936bb
bullhead242 <bullhead242@gmail.com>
2016-09-19 14:31:20 -0400

    Add matrix information and method calls to create "World" matrix

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,4 +1,4 @@
-#cool
+# cool
 # Dan's matrix extravaganza!!!
 # matrix library for Ruby
 # http://ruby-doc.org/stdlib-2.0.0/libdoc/matrix/rdoc/Matrix.html#method-c-build
@@ -6,13 +6,14 @@
 require 'matrix'
 
 class World
+  attr_reader :cells
   def initialize(length)
     @cells = Matrix.build(length,length) { |row, column| Cell.new }
   end
 
   # test code for neighborhood
-  def find_neighbors
-
+  def find_neighbors(x, y)
+    cells.[](x,y)
   end
 end
 
@@ -23,10 +24,17 @@ class Cell
     @column
   end
 
+  # count live neighbors (assume access to a neighbors array)
+  def count_live_neighbors
+    neighbors.filter().reduce(:+)
+  end
+
   def find_neighbors
     # world is a matrix
     # world.[](x, y) will give you an individual cell with coordinates x and y
     # world.index(cell) will return coordinates of that cell in the form [x, y]
-    
+    # does "find_neighbors" require a definition of neighbors to be iterated upon? (pete sez yes)
+
+
   end
 end
